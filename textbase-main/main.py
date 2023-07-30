c6c8c451551778ce16deacf50c5706914e6575cf
Sumit Raghuwanshi <sumitraghuwanshi7697@gmail.com>
2023-07-31 02:45:09 +0530

    updated the data for the converation in the txt file

--- textbase-main/main.py
+++ textbase-main/main.py
@@ -7,6 +7,7 @@ from textbase.message import Message
 import os
 import sys
 import logging
+import difflib
 from typing import List
 import importlib
 
@@ -17,87 +18,86 @@ models.OpenAI.api_key = os.getenv("OPENAI_API_KEY")
 
 # Prompt for GPT-3.5 Turbo
 SYSTEM_PROMPT = (
-    "You are chatting with an AI. There are no specific prefixes for responses, so you can ask or talk about anything you like. The AI will respond in a natural, conversational manner. Feel free to start the conversation with any question or topic, and let's have a pleasant chat!"
-    )
-# Options for interview preparation assistance
-INTERVIEW_OPTIONS = [
-    "1. Interview Tips",
-    "2. Resume Review",
-    "3. Common Interview Questions",
-    "4. Behavioral Interviews",
-    "5. Technical Interviews",
-]
+    "Welcome to the Interview Coach! I am here to help you with interview preparation. "
+    "Feel free to ask any interview-related questions or share your concerns. "
+    "Whether it's interview tips, common questions, or behavioral interviews, I've got you covered. "
+    "Ask away, and let's make sure you shine in your next interview!"
+)
 
-INTERVIEW_OPTIONS_FILE = [
-     "interviewtips.py",
-     "resumereview.py",
-     "commoninterviewquestoins.py",
-     "behavinterviews.py",
-     "techinterviews.py"
-]
-    
+def read_help():
+    file_path = os.path.join(os.path.dirname(__file__), "chatbot_help.txt")
+    with open(file_path, "r") as file:
+        help_text = file.read()
+    return help_text
 
-def get_module_from_file_path(file_path: str):
-    """
-    The function `get_module_from_file_path` takes a file path as input, loads the module from the file,
-    and returns the module.
+def read_interview_conversation():
+    file_path = os.path.join(os.path.dirname(__file__), "interview_tips.txt")
+    with open(file_path, "r") as file:
+        conversation_data = file.read()
+    return conversation_data
 
-    :param file_path: The file path is the path to the Python file that you want to import as a module.
-    It should be a string representing the absolute or relative path to the file
-    :type file_path: str
-    :return: the module that is loaded from the given file path.
-    """
-    module_name = os.path.splitext(os.path.basename(file_path))[0]
-    spec = importlib.util.spec_from_file_location(module_name, file_path)
-    module = importlib.util.module_from_spec(spec)
-    sys.modules[module_name] = module
-    spec.loader.exec_module(module)
-    return module
+def prepare_conversation_list(conversation_data):
+    pairs = conversation_data.strip().split("\n\n")
+    conversation_list = []
+    for pair in pairs:
+        user, bot = pair.split("\n")
+        conversation_list.append((user.replace("User: ", ""), bot.replace("Bot: ", "")))
+    return conversation_list
+
+def get_most_similar_response(user_input, conversation_list):
+    max_similarity = 0
+    most_similar_response = None
+
+    for user_msg, bot_msg in conversation_list:
+        similarity = difflib.SequenceMatcher(None, user_input.lower(), user_msg.lower()).ratio()
+        if similarity > max_similarity and similarity > 0.8:  # You can adjust this threshold based on your requirement
+            max_similarity = similarity
+            most_similar_response = bot_msg
+
+    return most_similar_response
 
 @textbase.chatbot("talking-bot")
 def on_message(message_history: List[Message], state: dict = None):
-    """Your chatbot logic here
-    message_history: List of user messages
-    state: A dictionary to store any stateful information
 
-    Return a string with the bot_response or a tuple of (bot_response: str, new_state: dict)
-    """
+    conversation_data = read_interview_conversation()
+    conversation_list = prepare_conversation_list(conversation_data)
     
     if state is None or "counter" not in state:
         state = {"counter": 0}
 
-    #Display the initial prompt at the begining of the message
     if not message_history:
         bot_response = SYSTEM_PROMPT
         return bot_response,state
     
     latest_message = message_history[-1]
 
-    if latest_message.content.strip().lower() in ["hi", "hello"]:
-        bot_response = "Hello! I am your Interview Coach. How can I assist you with your interview preparation today?"+"\n".join(INTERVIEW_OPTIONS)
+    if latest_message.content.strip().lower() in ["help", "commands", "info"]:
+        help_text = read_help()
+        bot_response = help_text
+        return bot_response, state
+
+    if latest_message.content.strip().lower() in ["hi", "hello","hey"]:
+        bot_response = "Hello! I am your Interview Coach. How can I assist you with your interview preparation today?\n"
         return bot_response , state
+    
+    else:
+        # Search for the most similar user message from the conversation list
+        user_input = latest_message.content.lower()  # Convert user input to lowercase
+        similar_response = get_most_similar_response(user_input, conversation_list)
 
-      # Check if the user has selected an option from the list
-    selected_option = latest_message.content.strip()
-    if selected_option.isdigit() and 1 <= int(selected_option) <= len(INTERVIEW_OPTIONS):
-        # Load the corresponding logic file for the selected option
-        option_index = int(selected_option) - 1
-        
-        logic_file = f"{INTERVIEW_OPTIONS_FILE[option_index]}"  # Replace this with your logic file naming convention
-        module = get_module_from_file_path(logic_file)
-
-        # Call the on_message function from the loaded module
-        response = module.on_message(message_history, state)
-        return response
-
+        if similar_response:
+           return similar_response, state
 
     state["counter"]+=1
 
-    # # Generate GPT-3.5 Turbo response
-    bot_response = models.OpenAI.generate(
-        system_prompt=SYSTEM_PROMPT,
-        message_history=message_history,
-        model="gpt-3.5-turbo",
-    )
+    try:
+        bot_response = models.OpenAI.generate(
+            system_prompt=SYSTEM_PROMPT,
+            message_history=message_history,
+            model="gpt-3.5-turbo",
+        )
+        return bot_response, state
+    except Exception as e:
+        bot_response = "Sorry, there was a problem processing your request. Please try again later."
+        return bot_response, state
 
-    return bot_response, state
